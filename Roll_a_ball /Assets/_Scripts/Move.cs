eccc89688d4fe4ef9460ae316b09ad1a9d6476c3
Christian Nogueras <christian_nogueras94@hotmail.com>
2016-10-04 23:27:49 -0400

    Added all the projects finished

---
+++ Roll_a_ball /Assets/_Scripts/Move.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// el nmonobehavior hereda las fisica y todo eso literalmente el game loop
+public class Move : MonoBehaviour {
+
+	// otro metodos que se pueden usar del gameLoop 
+	//void awake {} 
+	// void fixedUpdate 
+	//void LatedUpdate 
+
+
+
+	private Rigidbody player;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		// Gameobject mayusucula y miniscula 
+		// cojer player y cojer gameObject.getcomponenty anadale rigiBody
+		player = gameObject.GetComponent<Rigidbody> ();
+
+	
+	}
+
+
+
+
+
+
+
+	// Update is called once per frame
+	// para buscar infirmacion en iinternet es comand + '
+	// tools - editor syntaxHighliting
+	void FixedUpdate () {
+		float hMov = Input.GetAxis ("Horizontal");
+		float vMov = Input.GetAxis ("Vertical");
+
+		Vector3 Movement = new Vector3 (hMov,0,vMov);
+
+		player.AddForce (Movement * speed );
+
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "pickup"){
+			Destroy (other.gameObject);
+			//Debug.Log("collide);
+	}
+}
+
+}
+
---
+++ Roll_a_ball /Roll_a_ball copia/Assets/_Scripts/FollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+
+	private Vector3 offset;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+		// el spacio entre el jugador y la camera para hacer que la camara le siga
+		// ahy es donde esta la camara
+		offset = transform.position - target.position;
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// darle update a la posicion 
+		// miniscula el objeto al que esta pegao el script
+		transform.position = offset + target.position;
+
+	
+	}
+}
